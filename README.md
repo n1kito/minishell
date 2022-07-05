8b2338617f9a3b13d5626de497bb0f989fc43125
n1kito <mjallada@student.42.fr>
2022-07-05 10:52:06 +0200

    Cleaning up rules so they are closer to bash and easier to debug

--- README.md
+++ README.md
@@ -106,24 +106,24 @@ Shell reads and executes commands in the following way:
 
 ### Tokenizer process:
 
-- [x] If the end of input is recognized, the current token (if any) shall be delimited.
-- [x] OPERATORS: If the previous character was used as part of an operator and the current character is not quoted and can be used with the previous characters to form an operator, it shall be used as part of that (operator) token.
+- [x] (1) If the end of input is recognized, the current token (if any) shall be delimited.
+- [x] (2) If the previous character was used as part of an operator and the current character is not quoted and can be used with the previous characters to form an operator, it shall be used as part of that (operator) token.
   - [Shell operators](https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_260) are:
     - Control Operators: `&` `&&` `(` `)` `;` `;;` `newline` `|` `||` _(The end-of-input indicator used internally by the shell is also considered a control operator.)_
     - Redirection Operators: `<` `>` `>|` `<<` `>>` `<&` `>&` `<<-` `<>`
   - **According to the `minishell` subject, we have to handle the following operators**: `<` `>` `<<` `>>` and `|`
-- [x] OPERATORS: If the previous character was used as part of an operator and the current character cannot be used with the previous characters to form an operator, the operator containing the previous character shall be delimited.
-- [ ] If the current character is `<backslash>`, single-quote, or double-quote and it is not quoted, it shall affect quoting for subsequent characters up to the end of the quoted text. 
+- [x] (3) If the previous character was used as part of an operator and the current character cannot be used with the previous characters to form an operator, the operator containing the previous character shall be delimited.
+- [ ] (4) If the current character is `<backslash>`, single-quote, or double-quote and it is not quoted, it shall affect quoting for subsequent characters up to the end of the quoted text. 
   - The rules for quoting are as described in Quoting.
   - During token recognition no substitutions shall be actually performed, and the result token shall contain exactly the characters that appear in the input (except for <newline> joining), unmodified, including any embedded or enclosing quotes or substitution operators, between the <quotation-mark> and the end of the quoted text. The token shall not be delimited by the end of the quoted field.
   - The various quoting mechanisms are the escape character ` \ `, single-quotes `'`, and double-quotes `"`.
   - [More about UNIX Shell Quoting](https://rg1-teaching.mpi-inf.mpg.de/unixffb-ss98/quoting-guide.html)
-- [ ] If the current character is an unquoted '$' or '`', the shell shall identify the start of any candidates for parameter expansion (Parameter Expansion), command substitution (Command Substitution), or arithmetic expansion (Arithmetic Expansion) from their introductory unquoted character sequences: '$' or "${", "$(" or '`', and "$((", respectively. The shell shall read sufficient input to determine the end of the unit to be expanded (as explained in the cited sections). While processing the characters, if instances of expansions or quoting are found nested within the substitution, the shell shall recursively process them in the manner specified for the construct that is found. The characters found from the beginning of the substitution to its end, allowing for any recursion necessary to recognize embedded constructs, shall be included unmodified in the result token, including any embedded or enclosing substitution operators or quotes. The token shall not be delimited by the end of the substitution.
-- [ ] OPERATORS: If the current character is not quoted and can be used as the first character of a new operator, the current token (if any) shall be delimited. The current character shall be used as the beginning of the next (operator) token.
-- [x] If the current character is an unquoted `<blank>`, any token containing the previous character is delimited and the current character shall be discarded.
-- [ ] If the previous character was part of a word, the current character shall be appended to that word.
-- [ ] If the current character is a '#', it and all subsequent characters up to, but excluding, the next <newline> shall be discarded as a comment. The <newline> that ends the line is not considered part of the comment.
-- [ ] The current character is used as the start of a new word.
+- [ ] (5) If the current character is an unquoted '$' or '`', the shell shall identify the start of any candidates for parameter expansion (Parameter Expansion), command substitution (Command Substitution), or arithmetic expansion (Arithmetic Expansion) from their introductory unquoted character sequences: '$' or "${", "$(" or '`', and "$((", respectively. The shell shall read sufficient input to determine the end of the unit to be expanded (as explained in the cited sections). While processing the characters, if instances of expansions or quoting are found nested within the substitution, the shell shall recursively process them in the manner specified for the construct that is found. The characters found from the beginning of the substitution to its end, allowing for any recursion necessary to recognize embedded constructs, shall be included unmodified in the result token, including any embedded or enclosing substitution operators or quotes. The token shall not be delimited by the end of the substitution.
+- [ ] (6) If the current character is not quoted and can be used as the first character of a new operator, the current token (if any) shall be delimited. The current character shall be used as the beginning of the next (operator) token.
+- [x] (7) If the current character is an unquoted `<blank>`, any token containing the previous character is delimited and the current character shall be discarded.
+- [x] (8) If the previous character was part of a word, the current character shall be appended to that word.
+- [x] (9) ~~If the current character is a '#', it and all subsequent characters up to, but excluding, the next <newline> shall be discarded as a comment. The <newline> that ends the line is not considered part of the comment.~~
+- [ ] (10) The current character is used as the start of a new word.
 
 Once a token is delimited, it is categorized as required by the grammar in Shell Grammar.
 
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -127,101 +127,113 @@ void	extract_node(t_tokens **tokens, char *token_ptr, char *token_start, char *t
 {
 //	printf("extract #%d\n", ++extract);
 //	printf("token len: %d\n", (int)(token_end - token_start));
-	if (is_blank_character(*token_start) || is_blank_character(*token_end))
-		return ;
-	add_token_node(tokens, token_ptr, (int)(token_end - token_start + 1));
+	if (!is_blank_character(*token_start) || !is_blank_character(*token_end))
+		add_token_node(tokens, token_ptr, (int)(token_end - token_start + 1));
 //	printf("extracted \"%s\"\n", get_last_token(*tokens)->token);
+	printf("+ ");
+}
+
+int	is_part_of_token(char *ptr)
+{
+	if (is_blank_character(*ptr) || is_operator(*ptr))
+		return (0);
+	return (1);
 }
 
 void	tokenizer(char *line, t_tokens **tokens)
 {
 	int	token_start;
-	int	token_end;
-	int quoting;
+	int	current_char;
+//	int quoting;
 
 	token_start = 0;
-	token_end = 0;
-	quoting = 0;
-	while (token_end <= ft_strlen(line))
+	current_char = 0;
+//	quoting = 0;
+	while (current_char <= ft_strlen(line))
 	{
+		// 1
 		// If the end of input is recognized, the current token (if any) shall
 		// be delimited.
-		if (line[token_end] == '\0')
+		if (line[current_char + 1] == '\0')
 		{
-			if (token_end != token_start)
-				extract_node(tokens, &line[token_start], &line[token_start], &line[token_end] - 1);
-//			add_token_node(tokens, &line[token_start], token_end - 1 - token_start);
-//			token_end++;
-//			token_start = token_end;
+			printf("[Rule 1] ");
+			extract_node(tokens, &line[token_start], &line[token_start], &line[current_char]);
+			break ;
 		}
+		// 2
 		// If the previous character was used as part of an operator and the
 		// current character is not quoted and can be used with the previous
 		// characters to form an operator, it shall be used as part of that (operator) token.
-		else if (is_operator(line[token_end - 1]) && can_form_operator(&line[token_start], &line[token_end]))
+		else if (is_operator(line[current_char - 1]) && can_form_operator(&line[token_start], &line[current_char]))
 		{
-//			printf("#1\n");
-//			printf("token start is %p, token end is %p\n", &line[token_start], &line[token_end]);
-			extract_node(tokens, &line[token_start], &line[token_start], &line[token_end]);
-//			add_token_node(tokens, &line[token_start], token_end - token_start);
-//			token_start = token_end;
-//			printf("token_start is %c\ntoken_end is %c\n", line[token_start], line[token_end]);
-//			extract_node(tokens, &line[token_start], &token_start, &token_end);
-//			token_end++;
-			token_start = token_end + 1;
-			token_end = token_start;
+			printf("[Rule 2] ");
+//			extract_node(tokens, &line[token_start], &line[token_start], &line[current_char]);
+//			token_start = current_char + 1;
+//			current_char = token_start;
+			current_char++;
+			// I could replace this with token_start = ++current_char;
 		}
+		// 3
 		// If the previous character was used as part of an operator and the
 		// current character cannot be used with the previous characters to form
 		// an operator, the operator containing the previous character shall be delimited.
-		else if (is_operator(line[token_end - 1]) && !can_form_operator(&line[token_start], &line[token_end]))
+		else if (is_operator(line[current_char - 1]) && !can_form_operator(&line[token_start], &line[current_char]))
 		{
-//			printf("#2\n");
-//			printf("token_start is %c\ntoken_end is %c\n", line[token_start], line[token_end]);
-			extract_node(tokens, &line[token_start], &line[token_start], &line[token_end] - 1);
-//			add_token_node(tokens, &line[token_start], (token_end - 1) - token_start);
-			token_start = token_end;
+			printf("[Rule 3] ");
+			extract_node(tokens, &line[token_start], &line[token_start], &line[current_char] - 1);
+			token_start = current_char;
+//			current_char++;
 		}
-		// If the current character is <backslash>, single-quote, or double-quote
-		// and it is not quoted, it shall affect quoting for subsequent characters
-		// up to the end of the quoted text.
-		else if (is_quote_character(line[token_end]))
-		{
-//			printf("quote char found\n");
-			if (is_quote_character(line[token_start]) && token_end - token_start)
-				quoting = 0;
-			else
-				quoting = 1;
-		}
-		// If the current character is an unquoted '$' or '',
-		// TODO Find the actual rule because I have a doubt
+		// 4
 		//
-		// TODO and do something here
+		// 5
 		//
+		// 6
 		// If the current character is not quoted and can be used as the first
 		// character of a new operator, the current token (if any) shall be
 		// delimited. The current character shall be used as the beginning of
 		// the next (operator) token.
-		else if (is_operator(line[token_end]) && quoting == 0)
+		else if (is_operator(line[current_char]))
 		{
-//			printf("#3\n");
-//			printf("token_start is %c\ntoken_end is %c\n", line[token_start], line[token_end]);
-			if (token_end != token_start)
-				extract_node(tokens, &line[token_start], &line[token_start], &line[token_end - 1]);
-//			token_end++;
-			token_start = token_end;
+			printf("[Rule 6] ");
+			if (current_char && is_part_of_token(&line[current_char - 1])) // checks that we're not a the start of a line and that the previous char is not a blank
+			{
+				extract_node(tokens, &line[token_start], &line[token_start], &line[current_char - 1]);
+				token_start = current_char;
+			}
+//			current_char++;
 		}
-		else if (is_blank_character(line[token_end]) && quoting == 0)
+		// 7
+		// If the current character is an unquoted `<blank>`, any token containing
+		// the previous character is delimited and the current character shall be discarded.
+		else if (is_blank_character(line[current_char]))
 		{
-//			printf("found blank char @ %d\n", token_end);
-			extract_node(tokens, &line[token_start], &line[token_start], &line[token_end] - 1);
-			token_start = token_end + 1;
+			printf("[Rule 7] ");
+			if (current_char && is_part_of_token(&line[current_char - 1]))
+				extract_node(tokens, &line[token_start], &line[token_start], &line[current_char] - 1);
+			token_start = current_char + 1;
+			current_char = token_start;
+			// I could replace this with token_start = ++current_char;
 		}
-		//If the current character is a '#', it and all subsequent characters up to,
-		// but excluding, the next shall be discarded as a comment. The that ends
-		// the line is not considered part of the comment.
-		// TODO Implement this bullshit
-//		printf("token++\n");
-		token_end++;
+		// 8
+		// If the previous character was part of a word, the current character
+		// shall be appended to that word.
+		else if (is_part_of_token(&line[current_char - 1]))
+		{
+			printf("[Rule 8] ");
+			current_char++;
+		}
+		// 9
+		// I we don't handle this rule
+		//
+		// 10
+		// The current character is used as the start of a new word.
+		else
+		{
+			printf("[Rule 10] ");
+			token_start = current_char;
+			current_char++;
+		}
 	}
 }
 
@@ -236,6 +248,7 @@ int	main(int argc, char **argv)
 	{
 		printf(" %s\n", argv[1]);
 		tokenizer(argv[1], &tokens);
+		printf("\n");
 		print_tokens(tokens);
 	}
 	return (0);
