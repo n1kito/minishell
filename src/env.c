822090a8e4ac1c36d4e38fb4a37ee09c3e13fdf2
Victor RIGAUDY <vrigaudy@e1r8p2.clusters.42paris.fr>
2022-07-06 16:01:13 +0200

    segfault dans la copie de donnees a corriger

--- src/env.c
+++ src/env.c
@@ -6,21 +6,28 @@
 /*   By: vrigaudy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 17:25:51 by vrigaudy          #+#    #+#             */
-/*   Updated: 2022/07/05 19:51:44 by vrigaudy         ###   ########.fr       */
+/*   Updated: 2022/07/06 16:00:07 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../include/minishell.h"
+
+// This functions gives the number of env variables
+// It works similarly to the strlen function
 
 int	env_len(char **envp)
 {
 	int	i;
 
 	i = 0;
-	while (envp[i][0])
+	while (envp[i])
 		i++;
 	return (i);
 }
+
+// This function takes the linked list and writes the env variables in it
+// the first string called NAME takes the beggininning of the string until the =
+// the second string VARIABLE takes the rest of the string
 
 void	env_write(t_env *env, char **envp)
 {
@@ -31,7 +38,8 @@ void	env_write(t_env *env, char **envp)
 	i = 0;
 	j = 0;
 	k = 0;
-	while (env && envp[i][0])
+	printf("OK\n");
+	while (envp[i])
 	{
 		while (envp[i][j] && envp[i][j - 1] != '=')
 			env->name[k++] = envp[i][j++];
@@ -47,7 +55,10 @@ void	env_write(t_env *env, char **envp)
 	}
 }
 
-t_env	*list_init(t_env *env, int depth)
+// This function initializes the linked list
+// This list will be used to store the environment
+
+t_env	*list_init(int depth)
 {
 	t_env	*new;
 
@@ -56,9 +67,19 @@ t_env	*list_init(t_env *env, int depth)
 	new = malloc(sizeof(t_env *));
 	if (!new)
 		return (NULL);
-	new->next = list_init(new->next, depth - 1);
+	new->name = malloc(sizeof(char *));
+	if (!new->name)
+		return (NULL);
+	new->variable = malloc(sizeof(char *));
+	if (!new->variable)
+		return (NULL);
+	new->next = list_init(depth - 1);
 	return (new);
 }
+
+// This function takes the environment given as an argument
+// It then transforms it into a linked list with
+// It then returns a pointer to the first element of the list
 
 t_env	*get_env(char **envp)
 {
@@ -66,8 +87,24 @@ t_env	*get_env(char **envp)
 	t_env	*env;
 
 	len = env_len(envp);
-	env = list_init(env, len);
+	env = list_init(len);
 	env_write(env, envp);
+	printf("OK\n");
 	return (env);
 }
 
+int	main(int argc, char **argv, char **envp)
+{
+	t_env *env;
+
+	(void)argc;
+	(void)argv;
+	env = get_env(envp);
+	while (env)
+	{
+		printf("%s",env->name);
+		printf("%s\n",env->variable);
+		env = env->next;
+	}
+	return (0);
+}
