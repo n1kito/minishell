f69e256107a4dc87a385f709f6b1174544f1f4e5
Victor RIGAUDY <vrigaudy@e1r8p9.clusters.42paris.fr>
2022-07-14 18:14:16 +0200

    env to char** done. need testing

--- src/env_for_exe.c
+++ src/env_for_exe.c
@@ -6,16 +6,34 @@
 /*   By: vrigaudy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 18:04:10 by vrigaudy          #+#    #+#             */
-/*   Updated: 2022/07/13 19:22:49 by vrigaudy         ###   ########.fr       */
+/*   Updated: 2022/07/14 17:59:18 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// This functions takes a linked list where the env is stored
+// It transforms it into a char** for the sake of the execution
+
 char **env_for_exe(t_env *env)
 {
-	char **envp;
+	int		i;
+	char	**envp;
 
-	envp = malloc 
+	i = 0;
+	envp = malloc(sizeof(char *));
+	if (!envp)
+		return (NULL);
+	while (env)
+	{
+		*(envp + i) = malloc(sizeof(char) * (ft_strlen(env->name)
+			+ ft_strlen(env->variable) + 1));
+		if (!*(envp + i))
+			return (NULL);
+		ft_strlcpy(*(envp + i), env->name, ft_strlen(env->name));
+		ft_strlcat(*(envp + i), env->variable, ft_strlen(env->variable));
+		env = env->next;
+		i++;
+	}
 	return (envp);
 }
