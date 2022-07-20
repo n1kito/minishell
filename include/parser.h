b235f2ee854d35b521578e7f2700ecf29dafaa89
Nikito <mjallada@student.42.fr>
2022-07-20 14:19:24 +0000

    update parser

--- include/parser.h
+++ include/parser.h
@@ -11,7 +11,13 @@
 # define ASSIGNMENT_WORD 2
 # define NAME 3
 # define IO_NUMBER 4
+# define REDIRECT_FROM 5
+# define REDIRECT_TO 5
+# define APPEND 6
+# define HERE_DOC 7
+# define PIPE 8
 
-void	parser(t_tokens *tokens);
+void	parser(t_tokens **tokens);
+int		is_operator_token(t_tokens *tokens);
 
 #endif
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,6 +1,38 @@
 #include "../../include/parser.h"
 
-void	parser(t_tokens *tokens)
+void	parser(t_tokens **tokens)
 {
-		
+		t_tokens	*current;
+
+		current = *tokens;
+		while ((current))
+		{
+				if (!is_operator_token(current))
+						if (!is_io_number_token(current))
+								is_simple_token(current);
+				current = current->next;
+		}
+}
+
+int	is_operator_token(t_tokens *token)
+{
+		if (!is_operator(token->token[0]))
+						return (0);
+		if (ft_strlen(token->token) == 2)
+		{
+				if (token->token[0] == L_CHEVRON)
+						token->token_type = HERE_DOC;
+				else
+						token->token_type = APPEND;
+		}
+		else
+		{
+				if (token->token[0] == L_CHEVRON)
+						token->token_type = REDIRECT_FROM;
+				else if (token->token[0] == R_CHEVRON)
+						token->token_type = REDIRECT_TO;
+				else
+						token->token_type = PIPE;
+		}
+		return (1);
 }
