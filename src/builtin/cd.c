8aa96219ee16213a775f1b47c68f19ee0ebb6128
Victor RIGAUDY <vrigaudy@e1r8p14.clusters.42paris.fr>
2022-07-26 03:28:09 +0200

    cd almost done. tilde behaviour to be emulated

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,16 +6,35 @@
 /*   By: vrigaudy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 14:21:35 by vrigaudy          #+#    #+#             */
-/*   Updated: 2022/07/24 22:09:34 by vrigaudy         ###   ########.fr       */
+/*   Updated: 2022/07/26 03:10:32 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include <unistd.h>
 
+
+
 int	cd(char *path, t_env *env)
 {
-	int	ret;
+	int		ret;
+	char	*new_pwd;
 
-	ret = chdir(path);
+	if (!path)
+	{
+		while (ft_strncmp(env->name, "HOME=", 5) != 0)
+			env = env->next;
+		if (!env)
+			printf("cd: HOME not set\n");
+		ret = chdir(env->variable);
+	}
+	else if (strncmp(path, "~", 1) == 0 && ft_strlen(path) == 1)
+	{
+
+	}
+	else
+		ret = chdir(path);
+	if (!ret)
+		ft_update_pwd(env);
+	return (ret);
 }
