c8a58e1a7187718c776fb54d55e3119d49b100de
Nikito <mjallada@student.42.fr>
2022-08-02 12:11:48 +0000

    fixed segfault

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -20,10 +20,10 @@ int	is_io_number_token(t_tokens *token)
 {
 	if (is_only_digits(token->token))
 	{
-		if ((ft_strlen(token->previous->token) == 1
+		if (((token->previous && ft_strlen(token->previous->token) == 1)
 				&& ((token->previous->token[0] == '<')
 					|| token->previous->token[0] == '>'))
-			|| (ft_strlen(token->next->token) == 1
+			|| ((token->next && ft_strlen(token->next->token) == 1)
 				&& ((token->next->token[0] == '<')
 					|| token->next->token[0] == '>')))
 			{
