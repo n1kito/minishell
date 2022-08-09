94483b0954b0fe50ed974bebe76ed51847468a53
Victor RIGAUDY <vrigaudy@e1r2p22.clusters.42paris.fr>
2022-08-09 18:45:17 +0200

    inter push. export a finir

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: vrigaudy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 05:57:14 by vrigaudy          #+#    #+#             */
-/*   Updated: 2022/08/03 17:37:10 by vrigaudy         ###   ########.fr       */
+/*   Updated: 2022/08/07 12:35:49 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@ static char	*add_variable(char *str, int i, int j)
 
 	if (i == 0 || j == 0 || j - i <= 0)
 		return (NULL);
+	variable = malloc(sizeof(char) * j - i + 1);
+	if (!variable)
+		return (NULL);
+	while (*(str - 1) != '=')
+		str++;
+		return (NULL);
+	ft_strlcpy(variable, str, j - i);
+	return (variable);
 }
 
 static char	*add_name(char *str, int i)
@@ -37,21 +45,32 @@ static void	add_to_env(t_env *env, char *variable)
 {
 	int		i;
 	int		j;
+	int		exist;
 	t_env	*new;
+	t_env	*save;
 
 	i = 0;
 	j = 0;
-	new = malloc(sizeof(t_env *));
-	if (!new)
-		return ;
-	new->next = NULL;
-	while (env->next)
-		env = env->next;
-	env->next = new;
+	exist = 0;
+	save = env;
+	new = NULL;
 	while (variable[i] && variable[i - 1] != '=')
 		i++;
 	while (variable[j])
 		j++;
+	while (env)
+	{
+		if (ft_strncmp(env->name, variable, i) == 0\
+			&& ft_strlen(env->name) == 1)
+			exist = 1;
+		env = env->next;
+	}
+		if
+	new = malloc(sizeof(t_env *));
+	if (!new)
+		return ;
+	new->next = NULL;
+	env->next = new;
 	env->name = add_name(variable, i);
 	if (env->name)
 		env->variable = add_variable(variable, i, j);
