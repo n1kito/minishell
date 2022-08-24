bfd63e2176e10184e33dce004d82cb647e0af594
Victor Rigaudy <82155421+vrigaudy@users.noreply.github.com>
2022-08-24 13:28:21 +0200

    Update env_for_exe.c

--- src/env/env_for_exe.c
+++ src/env/env_for_exe.c
@@ -10,10 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
 
-// This functions takes a linked list where the env is stored
-// It transforms it into a char** for the sake of the execution
+#include "minishell.h"
+
+// This function destroys the char** that has been created for the excutiom
+// This function is noly executed if there is a malloc problem
+
+static void	destroy_env(char **envp, int lim)
+{
+	int	i;
+
+	i = 0;
+	while (i < lim)
+	{
+		free(envp[i]);
+		i++;
+	}
+	free(envp);
+}
+
+// This functions counts the number of elements in the env linked list
 
 static int	count_env(t_env *env)
 {
@@ -28,25 +44,35 @@ static int	count_env(t_env *env)
 	return (i);
 }
 
-char **env_for_exe(t_env *env)
+// This functions takes a linked list where the env is stored
+// It transforms it into a char** for the sake of the execution
+// It stores the pointer in the master struct
+// It returns 0 in case of a success and 1 in case of a failure
+
+int	env_for_exe(t_master *master)
 {
 	int		i;
 	char	**envp;
 
 	i = 0;
-	envp = malloc(sizeof(char *) * count_env(env));
+	envp = malloc(sizeof(char *) * count_env(master->env));
 	if (!envp)
-		return (NULL);
+		return (1);
 	while (env)
 	{
-		envp[i] = malloc(sizeof(char) * (ft_strlen(env->name)
+		envp[i] = malloc(sizeof(char) * (ft_strlen(master->env->name)
 			+ ft_strlen(env->variable) + 1));
 		if (!envp[i])
-			return (NULL);
-		ft_strlcpy(envp[i], env->name, ft_strlen(env->name));
-		ft_strlcat(envp[i], env->variable, ft_strlen(env->variable));
+		{
+			ft_destroy_env(envp, i);
+			return (1);
+		}
+		ft_strlcpy(envp[i], master->env->name, ft_strlen(master->env->name));
+		ft_strlcat(envp[i], master->env->variable, \
+			ft_strlen(master->env->variable));
 		i++;
 		env = env->next;
 	}
-	return (envp);
+	master->env_array = envp;
+	return (0);
 }
