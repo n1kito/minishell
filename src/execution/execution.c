1afcf2397510ed2f4b49553f13f3d6eb49a3dfa4
Nikito <mjallada@student.42.fr>
2022-08-30 16:14:15 +0000

    working on processes

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -1,5 +1,30 @@
 #include "minishell.h"
 
+void	file_checker(t_master *master, int i)
+{
+	t_command	*command;
+
+	command = master->commands[i];
+}
+
+void	plug_first_command(t_master *master, int i)
+{
+	file_checker(master, i);
+	command_error_check(master, i);
+	// add dup2 here
+}
+
+void	pipe_plugger(t_master *master, int i)
+{
+	if (i == 0)
+		plug_first_command(master, i);
+	else
+		plug_nth_command(master, i);
+	pipe_and_file_closer(master);
+	execve(master->commands[i]->cmd_path, master->commands[i]->cmd_array, master->env_array);
+	exit(free_master(master, 1));
+}
+
 int	exec_loop(t_master *master)
 {
 	int	i;
