85115d2c18ca3543cc3b7eaafe762c25913ceab4
Victor RIGAUDY <vrigaudy@e1r9p9.clusters.42paris.fr>
2022-09-05 18:45:20 +0200

    export + unset + exit : tests to be made

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: vrigaudy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 15:12:00 by vrigaudy          #+#    #+#             */
-/*   Updated: 2022/09/05 14:49:48 by mjallada         ###   ########.fr       */
+/*   Updated: 2022/09/05 18:43:05 by vrigaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,20 @@ int	ft_exit(t_master *master, int cmd_index)
 	variable = master->commands[cmd_index]->cmd_array;
 	if (master->processes[cmd_index] > 0)
 		ft_putstr_fd("exit\n", 0);
-	if (!variable[1])
-		g_minishexit = 0;
-	else if (!ft_is_num(variable[1]))
+	if (variable[1] && !ft_is_num(variable[1]))
 	{
 		ft_putstr_fd("Minishell: exit: ", 2);
 		ft_putstr_fd(variable[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
 		g_minishexit = 2;
 	}
-	else if (variable[2])
+	else if (variable[1] && variable[2])
 	{
 		ft_putstr_fd("Minishell: exit: too many arguments\n", 2);
 		g_minishexit = 1;
 		return (1);
 	}
-	else
+	else if (variable[1])
 		g_minishexit = ft_atoi(variable[1]);
 	exit(g_minishexit);
 }
